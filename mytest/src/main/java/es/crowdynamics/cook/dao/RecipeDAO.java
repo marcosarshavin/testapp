d445cad5ec80c82ca2191f2849590867af2c3f6c
Luis <luissaizalvarez@gmail.com>
2016-08-23 21:44:58 +0200

    Actualizacion 23/08/2016 metodos Select,Delete,Update

--- mytest/src/main/java/es/crowdynamics/cook/dao/RecipeDAO.java
+++ mytest/src/main/java/es/crowdynamics/cook/dao/RecipeDAO.java
@@ -1,10 +1,13 @@
 package es.crowdynamics.cook.dao;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.crowdynamics.cook.domain.Recipe;
 
@@ -14,15 +17,29 @@ public class RecipeDAO {
 	@PersistenceContext
 	private EntityManager entitymanager;
 
+	
 	@Transactional(rollbackFor=Exception.class)
-	public void createRecipe(String name)	{
-		Recipe recipe = new Recipe();
-		recipe.setName(name);
-		this.entitymanager.persist(recipe);
+	public void createRecipe(Recipe receta)	{
+		
+		this.entitymanager.persist(receta);
 	}
 	
-	public Recipe findByName(String name)	{
+	public @ResponseBody Recipe findByName(String name)	{
 		String query = "select o from Recipe o where o.name = :name";
 		return this.entitymanager.createQuery(query, Recipe.class).setParameter("name", name).getSingleResult();
+	}
+	public  Recipe findById(BigDecimal id)	{
+		return this.entitymanager.find(Recipe.class, id);
+				
+	}
+	@Transactional(rollbackFor=Exception.class)
+	public void deleteRecipe(Recipe receta)	{
+		
+		this.entitymanager.remove(receta);
+	}
+	@Transactional(rollbackFor=Exception.class)
+	public void updateRecipe(Recipe receta)	{
+		
+		this.entitymanager.merge(receta);
 	}
 }
--- mytest/src/main/java/es/crowdynamics/cook/web/RecipeController.java
+++ mytest/src/main/java/es/crowdynamics/cook/web/RecipeController.java
@@ -1,36 +1,58 @@
 package es.crowdynamics.cook.web;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.crowdynamics.cook.dao.RecipeDAO;
 import es.crowdynamics.cook.domain.Recipe;
+import es.crowdynamics.cook.services.RecipeService;
 
 @Controller
+@RequestMapping(value = "/Recipes")
 public class RecipeController {
-
+	
 	@Autowired
 	private RecipeDAO recipeDAO;
-	
-	@RequestMapping(value = "/create")
-	public ResponseEntity<String> createRecipe(@RequestParam String name)	{
+	@Autowired
+	private RecipeService recipeService;
+	@RequestMapping(method= RequestMethod.POST)
+	public ResponseEntity<String> createRecipe(@RequestBody Recipe receta)	{
 		
-		recipeDAO.createRecipe(name);
+		recipeDAO.createRecipe(receta);
 		
 		return new ResponseEntity<String>(HttpStatus.CREATED);
 	}
 	
-	@RequestMapping(value = "/find")
-	public @ResponseBody Recipe findRecipe(@RequestParam String name)	{
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody Recipe findRecipe(@RequestParam BigDecimal id)	{
 		
-		Recipe recipe = recipeDAO.findByName(name);
+		Recipe recipe = recipeDAO.findById(id);
 		
 		return recipe;
 	}
+	
+	@RequestMapping(method = RequestMethod.DELETE)
+	public ResponseEntity<String> deleteRecipe(@RequestParam BigDecimal id)	{
+		
+		recipeService.findAndRemove(id);
+		
+		return new ResponseEntity<String>(HttpStatus.OK);
+		}
+	
+	@RequestMapping( method = RequestMethod.PUT)
+	public ResponseEntity<String> updateRecipe (@RequestBody Recipe receta){
+		recipeService.updaterecipe(receta);
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+	
 	
 }
